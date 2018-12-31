927ae6bd06ad3bbce7b4a08746af4615e7513c6f
Mitch-Tyrer <mltcinematography@gmail.com>
2018-12-31 14:06:17 -0700

    added styles to components and inline styles in react

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
   }
 
   nameChangedHandler = (event) => {
-    this.setState( {
+    this.setState({
       persons: [
         { name: "Mitch", age: 30 },
         { name: "Cara", age: 22 },
@@ -35,11 +35,19 @@ class App extends Component {
   }
 
   render() {
+    const style = {
+      backgroundColor: "magenta",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer"
+    };
+
     return (
       <div className="App">
         <h1>Hi, I'm a react app!</h1>
         <p>THis is really working!</p>
-        <button onClick={() => this.switchNameHandler("Mitchell!!!!")}>Switch Name</button>
+        <button style={style} onClick={() => this.switchNameHandler("Mitchell!!!!")}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler.bind(this, "A Unicorn!")}>My Hobbies: Being Fly AF</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} changed={this.nameChangedHandler} />
