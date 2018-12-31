ada35079142aa341380af61cb10e85cbcce0d90a
Mitch-Tyrer <mltcinematography@gmail.com>
2018-12-31 13:54:08 -0700

    learned two way binding using imputs and state changes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
     persons: [
       { name: "Mitch", age: 30 },
       { name: "Cara", age: 22 },
+      { name: "Paul", age: 33 }
     ]
   }
 
@@ -18,6 +19,17 @@ class App extends Component {
       persons: [
         { name: newName, age: 30 },
         { name: "Cara", age: 22 },
+        { name: "Paul", age: 33 }
+      ]
+    });
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState( {
+      persons: [
+        { name: "Mitch", age: 30 },
+        { name: "Cara", age: 22 },
+        { name: event.target.value, age: 33 }
       ]
     });
   }
@@ -30,6 +42,7 @@ class App extends Component {
         <button onClick={() => this.switchNameHandler("Mitchell!!!!")}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler.bind(this, "A Unicorn!")}>My Hobbies: Being Fly AF</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} changed={this.nameChangedHandler} />
       </div>
     );
   }
