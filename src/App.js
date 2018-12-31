a9bb73ee70ade7d1c551efa1fbe03d5052d564f5
Mitch-Tyrer <mltcinematography@gmail.com>
2018-12-31 16:42:17 -0700

    added conditions for persons to be rendered by react

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
       { name: "Mitch", age: 30 },
       { name: "Cara", age: 22 },
       { name: "Paul", age: 33 }
-    ]
+    ],
+    showPersons: false
   }
 
   switchNameHandler = (newName) => {
@@ -34,6 +35,11 @@ class App extends Component {
     });
   }
 
+  togglePersonHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+  }
+
   render() {
     const style = {
       backgroundColor: "magenta",
@@ -47,10 +53,15 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a react app!</h1>
         <p>THis is really working!</p>
-        <button style={style} onClick={() => this.switchNameHandler("Mitchell!!!!")}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler.bind(this, "A Unicorn!")}>My Hobbies: Being Fly AF</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} changed={this.nameChangedHandler} />
+        <button style={style} onClick={this.togglePersonHandler}>Toggle Persons</button>
+        {
+          this.state.showPersons === true ? 
+          <div>
+            <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+            <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler.bind(this, "A Unicorn!")}>My Hobbies: Being Fly AF</Person>
+            <Person name={this.state.persons[2].name} age={this.state.persons[2].age} changed={this.nameChangedHandler} />
+          </div> : null
+        }
       </div>
     );
   }
