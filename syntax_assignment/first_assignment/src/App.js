bc9403be042e6f41cc5d94458959d01eb52d3d94
Mitch-Tyrer <mltcinematography@gmail.com>
2018-12-31 16:14:02 -0700

    fixed the two-way binding and moved inline styling to component based on solution

--- syntax_assignment/first_assignment/src/App.js
+++ syntax_assignment/first_assignment/src/App.js
@@ -6,34 +6,26 @@ import UserOutput from './UserOutput/UserOutput.js';
 class App extends Component {
   state = {
     users: [
-      {username: "Admin", password: "DickDick1"},
-      {username: "Cara", password: "DickDick2"},
+      { username: "Admin", password: "DickDick1" },
+      { username: "Cara", password: "DickDick2" },
     ]
   }
 
   changeUserHandler = (newUser) => {
     this.setState({
       users: [
-        {username: newUser.target.value, password: "DickDick1"},
-        {username: newUser.target.value, password: "DickDick2"},
+        { username: newUser.target.value, password: "DickDick1" },
+        { username: newUser.target.value, password: "DickDick2" },
       ]
-    })
+    });
   }
-  
+
 
   render() {
-    const style = {
-      backgroundColor: "red",
-      font: "inherit",
-      border: "1px solid blue",
-      padding: "8px",
-      cursor: "pointer"
-    }
-
-    return (
+   return (
       <div className="App">
-        <UserInput style={style} changed={this.changeUserHandler} />
-        <UserOutput username={this.state.users[0].username} />
+        <UserInput changed={this.changeUserHandler} currentName={this.state.users[0].username} />
+        <UserOutput username={this.state.users[0].username}></UserOutput>
         <UserOutput>Welcome!</UserOutput>
       </div>
     );
--- syntax_assignment/first_assignment/src/UserInput/UserInput.js
+++ syntax_assignment/first_assignment/src/UserInput/UserInput.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
 const userInput = (props) =>{
+
+    const style = {
+        font: "inherit",
+        border: "2px solid red",
+        padding: "8px",
+        cursor: "pointer"
+      }
+
+
     return (
         <div>
-            <input onChange={props.changed} type="text" value={props.username}></input>
+            <input style={style} onChange={props.changed} type="text" value={props.currentName}></input>
         </div>
     );
 }
