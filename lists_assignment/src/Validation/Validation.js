f07ca2232c595cac28b303f2ac0365f9e3fd41d5
Mitch-Tyrer <mltcinematography@gmail.com>
2018-12-31 18:12:46 -0700

    added input and state to find the length

---
+++ lists_assignment/src/Validation/Validation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const validation = (props) => {
+    return (
+        <div>
+            <p>length is {props.length}</p>
+        </div>
+    )
+}
+
+export default validation;
