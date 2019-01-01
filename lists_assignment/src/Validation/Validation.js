b5de122a30963c67aae07f3d838132d95ba62fc8
Mitch-Tyrer <mltcinematography@gmail.com>
2018-12-31 18:43:02 -0700

    added text too long or short if statement in Validation

--- lists_assignment/src/Validation/Validation.js
+++ lists_assignment/src/Validation/Validation.js
@@ -1,11 +1,30 @@
 import React from 'react';
 
 const validation = (props) => {
-    return (
-        <div>
-            <p>length is {props.length}</p>
-        </div>
-    )
+    if(props.length <= 5){
+        return (
+            <div>
+                <p>Text is too short!</p>
+            </div>
+        )
+    } else if(props.length >= 5) {
+        return (
+            <div>
+                <p>Text is Long Enough!</p>
+            </div>
+        )
+    }
 }
+
+
+
+
+// const validation = (props) => {
+//     return (
+//         <div>
+//              <p>length is {props.length <=5 ? "too short" : "long enough"}</p>
+//         </div>
+//     )
+// }
 
 export default validation;
