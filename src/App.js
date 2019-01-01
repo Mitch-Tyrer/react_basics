74efe1e6e3d5155fd4f87613c60c542abea256a4
Mitch-Tyrer <mltcinematography@gmail.com>
2019-01-01 14:23:33 -0700

    added radium and added a hover style to button using sudo selector

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person.js'
-
+import Radium from 'radium';
 
 class App extends Component {
   state = {
@@ -53,7 +53,14 @@ class App extends Component {
       font: "inherit",
       border: "1px solid blue",
       padding: "8px",
-      cursor: "pointer"
+      cursor: "pointer",
+      ':hover' : {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
+      
+                // with radium you can add sudo selectors as a string.
+                // because they start with a ':' they are not valid js property names
     }
 
     let persons = null;
@@ -72,6 +79,10 @@ class App extends Component {
         </div>
       );
       style.backgroundColor = "red";
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     }
 
     let classes = [];
@@ -94,4 +105,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App); // higher order component
