35fe9476b82342278de4bbf4e3034628a04f6be1
Mitch-Tyrer <mltcinematography@gmail.com>
2019-01-01 18:48:38 -0700

    added Aux component to remove redundent jsx elements and explore higher order components (hoc)

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,31 +1,32 @@
 import React from 'react';
 import classes from './Cockpit.css';
+import Aux from '../../hoc/Auxiliary/Auxiliary';
 
 
 const cockpit = (props) => {
     let assignClasses = [];
-    let btnClass = '';
-    
-    if(props.showPersons) {
-        btnClass = classes.red;
+    let btnClass = classes.Button;
+
+    if (props.showPersons) {
+        btnClass = [classes.Button, classes.red].join(' ');
     }
 
-    if(props.persons.length <= 2) {
-      assignClasses.push(classes.red); // Classes will be ['red']
+    if (props.persons.length <= 2) {
+        assignClasses.push(classes.red); // Classes will be ['red']
     }
 
-    if(props.persons.length <= 1){
-      assignClasses.push(classes.bold); // classes will be ['red', 'bold']
+    if (props.persons.length <= 1) {
+        assignClasses.push(classes.bold); // classes will be ['red', 'bold']
     }
 
 
     return (
-        <div className={classes.Cockpit}>
-        <h1>Hi, I'm a react app!</h1>
-        <p className={assignClasses.join(' ')}>THis is really working!</p>
-        <button className={btnClass} 
-        onClick={props.clicked}>Toggle Persons</button>
-        </div>
+        <Aux>
+            <h1>Hi, I'm a react app!</h1>
+            <p className={assignClasses.join(' ')}>THis is really working!</p>
+            <button className={btnClass}
+                onClick={props.clicked}>Toggle Persons</button>
+        </Aux>
     );
 };
 
