542c5873ee3d835bb91419380a0b5872f4ddbe1b
Mitch-Tyrer <mltcinematography@gmail.com>
2019-01-01 19:06:04 -0700

    a different approach to higher order functions.  Lost props

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,9 @@ import React, { PureComponent } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
-import WithClass from '../hoc/WithClass';
+import Aux from '../hoc/Auxiliary/Auxiliary';
+import withClass from '../hoc/withClass';
+
 class App extends PureComponent {
 
   constructor(props) {
@@ -98,7 +100,7 @@ class App extends PureComponent {
 
 
     return (
-      <WithClass classes={classes.App}>
+      <Aux>
         <button onClick={() => { this.setState({ showPersons: true }) }}>Show Persons</button>
         <Cockpit
           showPersons={this.state.showPersons}
@@ -106,9 +108,9 @@ class App extends PureComponent {
           clicked={this.togglePersonHandler}
         />
         {persons}
-      </WithClass>
+      </Aux>
     )
   }
 }
 
-export default App;
+export default withClass(App, classes.App);
