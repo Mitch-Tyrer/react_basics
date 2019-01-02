0087afe57cbbef949e5d7aa0de9517c4e8f81f2f
Mitch-Tyrer <mltcinematography@gmail.com>
2019-01-01 18:12:19 -0700

    updated app.js to illustrate update lifcycle internally

--- src/containers/App.js
+++ src/containers/App.js
@@ -26,6 +26,20 @@ class App extends Component {
     console.log("[App.js] Inside componentDidMount()");
   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("[UPDATE App.js] Inside shouldComponentUpdate", nextProps, nextState);
+    return true;
+  }
+
+  componentWillUpdate(nextProps, nextState){
+      console.log('[UPDATE App.js] Inside componentWillUpdate()', nextProps, nextState);
+  }
+
+  componentDidUpdate() {
+    console.log("[UPDATE App.js] Inside componentDidUpdate()");
+  }
+
+
 /*   state = {
     persons: [
       { id: "asds1", name: "Mitch", age: 30 },
